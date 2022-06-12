2f3d27f07fdf3cf61b1548a81e36aeb5eea3cee9
NehalH <nhosalikar@gmail.com>
2022-06-12 18:20:14 +0530

    signature added

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/material/bottom_sheet.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-
-var authIcon = const Icon(Icons.lock, color: Colors.redAccent,);
-late String enteredPassKEY;
-
+String signature= 'Rogue/Outsider';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'Aerophilia 2022 Organizer',
-    initialRoute: '/',
-    routes: {
-      '/': (context) => const MyHomePage(),
-    }
-  ));
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Organizer',
+      initialRoute: '/home',
+      routes: {
+        '/home': (context) => const MyHomePage(),
+      },
+    ),
+  );
 }
 
 class MyHomePage extends StatefulWidget {
@@ -27,12 +24,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final TextEditingController _textInputController = TextEditingController();
+  bool authenticated = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _getVarSharedPref();
+  }
+
+  @override
+  void dispose() {
+    _textInputController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      backgroundColor: Colors.black54,
+      backgroundColor: Colors.white,
       appBar: AppBar(
         //leading: const Icon(Icons.account_circle_rounded),
         //leadingWidth: 80,
@@ -49,64 +59,119 @@ class _MyHomePageState extends State<MyHomePage> {
                 onLongPress: (){
                   _authDialog(context);
                 },
-                child: authIcon
+                child: _getAuthIcon(),
               )
           ),
         ],
       ),
+
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: MaterialButton(
+          height: 60,
+          onPressed: (){},//=====================================================================================
+          color: Colors.black,
+          child: const Icon(Icons.home, color: Colors.white,),
+        ),
+      ),
+
       body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: MaterialButton(
+                      height: 60,
+                      onPressed: (){},//=====================================================================================
+                      color: Colors.blueGrey,
+                      child: const Icon(Icons.home, color: Colors.white,),
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
-  
+
   void _authDialog(context) {
     showModalBottomSheet(
-        backgroundColor: Colors.black54,
+        backgroundColor: Colors.transparent,
         context: context,
         builder: (BuildContext context) {
           return SafeArea(child: Wrap(
-
             children: <Widget>[
               Dialog(
-                backgroundColor: Colors.black54,
-                elevation: 0,
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    const SizedBox(height: 80),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-                      child: TextField(
-                        onChanged: (value) =>  enteredPassKEY = value,
-                        //cursorColor: Colors.redAccent,
-                        textAlign: TextAlign.center,
-                        decoration: const InputDecoration(
-                          enabledBorder: OutlineInputBorder(
-                            borderSide: BorderSide(color: Colors.grey, width: 1.0),
-                          ),
-                          fillColor: Colors.white,
-                          focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.redAccent, width: 2.0),),
-                          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 1.0),),
-                          border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white,),),
-                          hintText: 'Enter Passkey',
-                          hintStyle: TextStyle(
-                            color: Colors.grey,
-                            fontSize: null,
-                            fontWeight: FontWeight.w400,
-                            fontStyle: FontStyle.normal,
+                  backgroundColor: Colors.black54,
+                  elevation: 0,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: <Widget>[
+                      const SizedBox(height: 20),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+                        child: TextField(
+                          controller: _textInputController,
+                          style: const TextStyle(color: Colors.transparent),
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                            ),
+                            fillColor: Colors.white,
+                            focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.redAccent, width: 2.0),),
+                            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 1.0),),
+                            border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white,),),
+                            hintText: 'Enter Passkey',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: null,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    const SizedBox(height: 10),
-                    MaterialButton(
-                      onPressed: () { checkPassKey(); },
-                      color: Colors.redAccent.shade400,
-                      child: const Text("Validate", style: TextStyle(color: Colors.white),),
-                    ),
-                    const SizedBox(height: 200),
-                  ],
-                )
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 5, 20, 10),
+                        child: TextField(
+                          onChanged: (value) => signature = value,
+                          textAlign: TextAlign.center,
+                          decoration: const InputDecoration(
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey, width: 1.0),
+                            ),
+                            fillColor: Colors.white,
+                            focusedErrorBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.redAccent, width: 2.0),),
+                            focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.white, width: 1.0),),
+                            border: OutlineInputBorder(borderSide: BorderSide(color: Colors.white,),),
+                            hintText: 'Signature',
+                            hintStyle: TextStyle(
+                              color: Colors.grey,
+                              fontSize: null,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                          child: MaterialButton(
+                            onPressed: () { checkPassKey(); },
+                            color: Colors.redAccent.shade400,
+                            child: const Text("Validate", style: TextStyle(color: Colors.white),),
+                          ),
+                      ),
+                    ],
+                  )
               ),
             ],
           ));
@@ -114,35 +179,36 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Future<void> _setPassKEYSharedPref() async{
-    final prefs= await SharedPreferences.getInstance();
-    await prefs.setString('passKEY', 'enteredPassKEY');
+  Future<void> _setPassKEYSharedPref() async {
+    final prefs = await SharedPreferences.getInstance();
+    // It doesn't make much sense imho, better use a boolean
+    await prefs.setString('passKEY', 'nehal');
   }
-
-/*  Future<void> _setAuthIconSharedPref() async{
-    final prefs= await SharedPreferences.getInstance();
-    await prefs.setString('authIcon', '$authIcon');
-  }*/
 
   Future<void> _getVarSharedPref() async {
-    final prefs= await SharedPreferences.getInstance();
-    authIcon = const Icon(Icons.lock, color: Colors.redAccent,);
-    enteredPassKEY = prefs.getString('passKEY')!;
-    // ignore: prefer_conditional_assignment, unnecessary_null_comparison
-    if(authIcon== null){
-      authIcon = const Icon(Icons.lock, color: Colors.redAccent,);
-      enteredPassKEY = 'not_authorised_yet';
-    }
+    final prefs = await SharedPreferences.getInstance();
+    final storedPasskey = prefs.getString('passKEY') ?? 'Not_yet_authorised';
+    setState(() {
+      authenticated = storedPasskey == 'nehal';
+    });
   }
 
-  void checkPassKey(){
-    if(enteredPassKEY=='Nehal'){
+  void checkPassKey() {
+    final String enteredPassKey = _textInputController.text;
+    if (enteredPassKey == 'nehal') {
       setState(() {
-        authIcon= const Icon(Icons.check_circle_sharp, color: Colors.greenAccent,);
+        authenticated = true;
       });
       _setPassKEYSharedPref();
-      //_setAuthIconSharedPref();
+    }
+    else {
+      signature = 'Rogue/Outsider';
     }
   }
 
+  Icon _getAuthIcon() {
+    return authenticated
+        ? const Icon(Icons.check_circle_sharp, color: Colors.greenAccent)
+        : const Icon(Icons.lock, color: Colors.redAccent);
+  }
 }
