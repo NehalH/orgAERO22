009bc880b02bae365a2b2d9f206422b9b02c0066
NehalH <nhosalikar@gmail.com>
2022-09-12 00:15:05 +0530

    flash added

--- lib/qr_scanner.dart
+++ lib/qr_scanner.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_switch/flutter_switch.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'globals.dart' as global;
 
@@ -16,6 +17,7 @@ class _ScanQrPageState extends State<ScanQrPage> {
   Barcode? result;
   QRViewController? controller;
   final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  bool flashStatus = false;
 
   // In order to get hot reload to work we need to pause the camera if the platform
   // is android, or resume the camera if the platform is iOS.
@@ -34,7 +36,7 @@ class _ScanQrPageState extends State<ScanQrPage> {
       body: Column(
         children: <Widget>[
           Expanded(flex: 4, child: _buildQrView(context)),
-          Expanded(
+          /*Expanded(
             flex: 1,
             child: FittedBox(
               fit: BoxFit.contain,
@@ -115,25 +117,68 @@ class _ScanQrPageState extends State<ScanQrPage> {
                 ],
               ),
             ),
-          )
+          )*/
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startFloat,
+      floatingActionButton: SafeArea(
+        child:  FloatingActionButton(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          child: FlutterSwitch(
+            width: 65.0,
+            height: 35.0,
+            toggleSize: 20.0,
+            value: flashStatus,
+            borderRadius: 30.0,
+            padding: 4.0,
+            activeToggleColor: Colors.white,
+            inactiveToggleColor: global.orange,
+            activeSwitchBorder: Border.all(
+              color: Colors.white,
+              width: 1.0,
+            ),
+            inactiveSwitchBorder: Border.all(
+              color: global.orange,
+              width: 1.0,
+            ),
+            activeColor: global.orange,
+            inactiveColor: Colors.white38,
+            activeIcon: Icon(Icons.lightbulb_rounded,color: global.orange,),
+            inactiveIcon: Icon(Icons.lightbulb_outline_rounded),
+            onToggle: (val) async {
+              flashStatus= val;
+              await controller?.toggleFlash();
+              setState(() {});
+            },
+          ),
+          onPressed: (){},
+        ),
       ),
     );
   }
 
   Widget _buildQrView(BuildContext context) {
     // For this example we check how width or tall the device is and change the scanArea and overlay accordingly.
-    var scanArea = (MediaQuery.of(context).size.width < 400 ||
-        MediaQuery.of(context).size.height < 400)
+    controller?.resumeCamera();
+    var scanArea = (MediaQuery
+        .of(context)
+        .size
+        .width < 400 ||
+        MediaQuery
+            .of(context)
+            .size
+            .height < 400)
         ? 150.0
         : 300.0;
+
     // To ensure the Scanner view is properly sizes after rotation
     // we need to listen for Flutter SizeChanged notification and update controller
     return QRView(
       key: qrKey,
       onQRViewCreated: _onQRViewCreated,
       overlay: QrScannerOverlayShape(
-          borderColor: Colors.red,
+          borderColor: global.orange,
           borderRadius: 10,
           borderLength: 30,
           borderWidth: 10,
@@ -149,7 +194,7 @@ class _ScanQrPageState extends State<ScanQrPage> {
     controller.scannedDataStream.listen((scanData) {
       setState(() {
         result = scanData;
-        global.scanID= result!.code;
+        global.scanID = result!.code;
       });
     });
   }
@@ -182,7 +227,7 @@ class _ScanQrPageState extends State<ScanQrPage> {
                 getStatusIcon(),
                 getStatusString(),
                 ElevatedButton(
-                    onPressed: (){
+                    onPressed: () {
                       Navigator.of(context).pop();
                     }, child: const Text("Close"))
               ],
@@ -193,30 +238,31 @@ class _ScanQrPageState extends State<ScanQrPage> {
     );
   }
 
-  Icon getStatusIcon(){
-    if(global.regStatus==1) {
-      return const Icon(Icons.verified_user,color: Colors.green,size: 180);
+  Icon getStatusIcon() {
+    if (global.regStatus == 1) {
+      return const Icon(Icons.verified_user, color: Colors.green, size: 180);
     }
-    else if(global.regStatus==0){
-      return const Icon(Icons.dangerous_rounded,color: Colors.red,size: 180,);
+    else if (global.regStatus == 0) {
+      return const Icon(Icons.dangerous_rounded, color: Colors.red, size: 180,);
     }
-    else if(global.regStatus==2){
-      return const Icon(Icons.warning_rounded,color: Colors.yellow,size: 180,);
+    else if (global.regStatus == 2) {
+      return const Icon(
+        Icons.warning_rounded, color: Colors.yellow, size: 180,);
     }
-    return const Icon(Icons.refresh,color: Colors.blueGrey,size: 180,);
+    return const Icon(Icons.refresh, color: Colors.blueGrey, size: 180,);
   }
 
-  Text getStatusString(){
-    if(global.regStatus==1) {
-      return const Text("Verified!",style:TextStyle(fontSize: 20),);
+  Text getStatusString() {
+    if (global.regStatus == 1) {
+      return const Text("Verified!", style: TextStyle(fontSize: 20),);
     }
-    else if(global.regStatus==0){
-      return const Text("Not Registered!",style:TextStyle(fontSize: 20),);
+    else if (global.regStatus == 0) {
+      return const Text("Not Registered!", style: TextStyle(fontSize: 20),);
     }
-    else if(global.regStatus==2){
-      return const Text("Payment Not done!",style:TextStyle(fontSize: 20),);
+    else if (global.regStatus == 2) {
+      return const Text("Payment Not done!", style: TextStyle(fontSize: 20),);
     }
-    return const Text("Error! Retry.",style:TextStyle(fontSize: 20),);
+    return const Text("Error! Retry.", style: TextStyle(fontSize: 20),);
   }
 
 }
